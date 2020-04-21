620f16f0af59b36cd6c67ad30d8454c44716ebfd
Ekksvar <jeremytr116@gmail.com>
2020-04-21 10:56:05 -0400

    Seeds Wouldn't Plant

--- src/main/java/com/ekksvar/ediblepumpkinseeds/init/ItemInit.java
+++ src/main/java/com/ekksvar/ediblepumpkinseeds/init/ItemInit.java
@@ -2,6 +2,8 @@ package com.ekksvar.ediblepumpkinseeds.init;
 
 import com.ekksvar.ediblepumpkinseeds.EdiblePumpkinSeeds;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockNamedItem;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -21,7 +23,8 @@ public class ItemInit
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) 
 	{
-		event.getRegistry().register(new Item(new Item.Properties().group(Items.PUMPKIN_SEEDS.getGroup()).food(new Food.Builder().hunger(1).fastToEat().build())).setRegistryName(Items.PUMPKIN_SEEDS.getRegistryName()));
+		event.getRegistry().register(new BlockNamedItem(Blocks.PUMPKIN_STEM, (new Item.Properties()).group(Items.PUMPKIN_SEEDS.getGroup()).food(new Food.Builder().hunger(1).fastToEat().build())).setRegistryName(Items.PUMPKIN_SEEDS.getRegistryName()));
+		//event.getRegistry().register(new BlockNamedItem(Blocks.PUMPKIN_STEM, (new Item.Properties()).group(Items.PUMPKIN_SEEDS.getGroup()).food(new Food.Builder().hunger(1).fastToEat().build())).setRegistryName(Items.PUMPKIN_SEEDS.getRegistryName()));
 		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.FOOD).food(new Food.Builder().hunger(2).saturation(0.3f).fastToEat().build())).setRegistryName("toasted_pumpkin_seeds"));
 	}
 }
